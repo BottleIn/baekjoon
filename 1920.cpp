681aad7bcc44ab430b9bea090c60e10a432cb1db
Jeun Byeongin <alvin1115@naver.com>
2025-02-18 15:27:49 +0900

    1

--- 1920.cpp
+++ 1920.cpp
@@ -1,50 +1,50 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 #define endl "\n"
-#define MAX 10002
+#define MAX 100000
 
 using namespace std;
-typedef long long ll;
 
-int N,M;
-int l,r; // 왼쪽 오른쪽 포인터
-int cnt, sum;
-int num[MAX];
+int dp1[MAX + 1];  // 1부터 사용할 배열
+int N, M;
 
-int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    
-    cin >> N >> M;
-    
-    for (int i = 1; i <= N; i++){
-        cin >> num[i];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 1; i <= N; i++) {  // 1부터 저장
+        cin >> dp1[i];
     }
 
-    sum = num[l];
+    // dp1[1]부터 dp1[N]까지 정렬 (dp1[0]은 무시)
+    sort(dp1 + 1, dp1 + N + 1);
 
-    while(l<=r && r<=N){
-        if(sum == M){
-            cnt++;
-            sum += num[++r];
-        }
-        else if( sum < M){
-            sum += num[++r];
-        }
-        else if( sum > M){
-            sum -= num[l++];
+    cin >> M;
+    for (int i = 1; i <= M; i++) {
+        int tar;
+        cin >> tar;
+
+        int start = 1, end = N, ans = 0;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (dp1[mid] == tar) {
+                ans = 1;
+                break;
+            }
+            else if (tar < dp1[mid]) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
         }
 
-        if (l > r) {        // 오름차순이 아니므로 넘어가면 리셋하기
-			r = l;
-			sum = num[l];
-		}
-
+        cout << ans << endl;
     }
-
-
-    cout << cnt << endl;
 
     return 0;
 }
